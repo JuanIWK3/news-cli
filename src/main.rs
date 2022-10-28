da651978a1d708f79ae793ea9c68396ef9b391fa
juaniwk3 <juaniwk3@gmail.com>
2022-10-28 03:06:11 -0300

    country selector

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use std::error::Error;
 
+use std::io;
 use serde::Deserialize;
 use colour::{blue, yellow};
+
 
 #[derive(Deserialize, Debug)]
 struct Articles {
@@ -31,8 +33,18 @@ fn render_articles(articles: &Articles) {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let url = "https://newsapi.org/v2/top-headlines?country=br&apiKey=9b95c40e64f24acea565aa175b6efdce&pageSize=4";
-    let articles = get_articles(url)?;
+    let countries = "Countries: 'ar', 'at', 'au', 'be', 'bg', 'br', 'ca', 'ch', 'cn', 'co', 'cu', 'cz', 'de', 'eg', 'fr', 'gb', 'gr', 'hk', 'hu', 'id', 'ie', 'il', 'jp', 'kr', 'lt', 'lv', 'ma', 'mx', 'my', 'ng', 'nl', 'no', 'nz', 'ph', 'pl', 'pt', 'ro', 'rs', 'ru', 'sa', 'se', 'sg', 'si', 'sk', 'th', 'tr', 'tw', 'ua', 'us', 've', 'za'";
+    println!("From which country do you want the news?");
+    println!("{}", countries);
+
+    let mut country = String::new();
+    let stdin = io::stdin();
+
+    stdin.read_line(&mut country)
+        .expect("Didn't Receive Input");
+
+    let url = "https://newsapi.org/v2/top-headlines?country=".to_owned() + &country + "&apiKey=9b95c40e64f24acea565aa175b6efdce&pageSize=4";
+    let articles = get_articles(&url)?;
     render_articles(&articles);
 
 
